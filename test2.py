a0aa38fe3a3708218d9f4d1aeb9970c36aff0b89
Ryo Nozaki <ryoooo1128@gmail.com>
2018-03-29 22:52:47 -0700

    test2.py

--- test2.py
+++ test2.py
@@ -1,1 +1,21 @@
-print 1 + 1
+import numpy as np
+x = np.array([0, 1])
+w = np.array ([0.5, 0.5])
+b = -0.7
+print w * x
+
+print np.sum(w * x)
+print np.sum(w * x) + b
+
+
+def AND(x1, x2):
+	x = np.array([x1, x2])
+	w = np.array([0.5, 0.5])
+	b = -0.7
+	tmp = np.sum(x * w) + b
+	if tmp <= 0:
+		return 0
+	else :
+		return 1
+
+print AND (0, 1)
