ab03b0bb72cca1a4accc6ff9795904b34b349184
Ryo Nozaki <ryoooo1128@gmail.com>
2018-04-08 16:00:21 -0700

    3_1_neural_network.py

--- 3_1_neural_network.py
+++ 3_1_neural_network.py
@@ -1,16 +1,20 @@
 import numpy as np
 import matplotlib.pylab as plt
 
+
+#ステップ関数:一定値を超えると1を返す
 def step_function(x):
 	return np.array(x > 0, dtype=np.int)
 
-x = np.arange(-5.0, 5.0, 0.1)
+x = np.arange(-5.0, 5.0, 0.1)#-0.5から0.5まで0.1ずつ
 y = step_function(x)
 plt.plot(x, y)
 plt.ylim(-0.1, 1.1)
 plt.show()
 
 
+
+#シグモイド関数:
 def sigmoid(x):
 	return 1 / (1 + np.exp(-x))
 
@@ -21,6 +25,8 @@ plt.ylim(-0.1, 1.1)
 plt.show()
 
 
+
+#ReLU関数:0を超えるとその値を返す
 def relu(x):
 	return np.maximum(0, x)
 
@@ -31,17 +37,3 @@ plt.ylim(-0.1, 5.1)
 plt.show()
 
 
-A = np.array([1, 2, 3, 4])
-print (A)
-print np.ndim(A)
-print A.shape
-print A.shape[0]
-
-B = np.array([[1, 2, 3], [4, 5, 6]])
-print (B)
-print np.ndim(B)
-print B.shape
-
-C = np.array([[1, 2], [3, 4]])
-D = np.array([[5, 6], [7, 8]])
-print np.dot(C, D)
