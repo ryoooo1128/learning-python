798bb7a63e6a7d5fb80c9b5ad9432ffe5d209410
Ryo Nozaki <ryoooo1128@gmail.com>
2018-04-28 00:36:17 -0700

    data_science_9.py

--- data_science/data_science_9.py
+++ data_science/data_science_9.py
@@ -1,12 +1,15 @@
 import sys, re
 import collections
-
+"""
 def process(date, symbol, price):
         print(date, symbol, price)
 
 #sys.argv：コマンドラインの引数のリスト
 #sys.argv[0]：プログラム名
 #sys.argv[1]：コマンドライン上で指定した正規表現
+
+
+
 
 
 #正規表現に合致した各行を出力
@@ -45,6 +48,8 @@ for word , count in counter.most_common(num_words):
 	sys.stdout.write("\n")
 
 #ここまではegrep.pyと同様
+
+
 
 
 
@@ -110,6 +115,9 @@ with open('comma_delimited_stock_prices.txt', 'w') as f:
 		writer.writerow([stock, price])
 
 
+
+
+
 #HTMLとその解析
 #pythonに組み込まれてるものは寛容でないためインストール
 #htmlパーサー：html5lib
@@ -146,6 +154,86 @@ spans_inside_divs = [span
 					for span in soup('span')]
 
 
+
+"""
+from bs4 import BeautifulSoup
+import requests
+
+
+#事例1
+url = "https://www.safaribooksonline.com/search/?query=data"
+soup = BeautifulSoup(requests.get(url).text, 'html5lib')
+
+#thumbtextタグを検索する
+tds = soup('td', 'thumbtext')
+print(len(tds))#30
+
+#Videoを取り除く　span内にある
+def is_video(td):#videoであるものを数える
+	pricelabels = td('span', 'pricelabel')
+	return len(pricelabels) == 1 and pricelabels[0].text.strip().startswish("Video")
+	#Video：pricelabelが、空白を取り除いてVideoで始まるもの１つしか持たない
+
+#情報収集の準備
+def book_info(td):#tdタグに書籍のデータが入っているため、取り出して辞書にする
+	#題名を取り出す　<div class="thumbheader"><a>
+	title = td.find("div", "thumbheader").a.text
+
+	#AuthorNameを取り出す　<div class="AuthorName"> By が前置されている
+	title = td.find("div", "thumbheader").a.text
+	author_name = td.find('div', 'AuthorName').text
+	authers = [x.strip() for x in re.sub("^By ", "", author_name).split(",")]#re.sub(正規表現 ^は最初が一致を意味, 置換する文字列, 置換される文字列)
+
+	#ISBNを取り出す　<div class="thumbheader"><a herf="/product/~.do">
+	isbn_link = td.find("div", "thumbheader").a.get("href")
+	isbn = re.match("/product/(.*)\.do", isbn_link).group(1)
+	#findは正規表現が指定できないためre.matchを使う
+	#.は任意の文字　*はその繰り返し
+
+	#日付を取り出す　<span class="directorydate">
+	date = td.find("span", "directorydate").text.strip()#stripは取り除く
+
+	return {
+		"title" : title,
+		"author" : authors,
+		"isbn" : isbn,
+		"date" : date
+	}
+
+
+
+from bs4 import BeautifulSoup
+import requests
+from time import sleep
+url = "https://www.safaribooksonline.com/search/?query=data"
+
+
+books =[]
+NUM_PAGES = 30
+
+"""for page_num in range(1, NUM_PAGES + 1):
+	print("souping page", page_num, ",", len(books), " found so far")
+	url = base_url + str(page_num)
+	soup = BeautifulSoup(requests.get(url).text, 'html5lib')
+"""
+soup = BeautifulSoup(requests.get(url).text, 'html5lib')
+for td in soup("td", "thumbheader"):
+	if not is_video(td):
+		books.append(book_info(td))
+
+def get_year(book):#dateの値はNovember 2014のようになっている
+	return int(book["date"].split()[1])
+
+year_counts = collections.Counter(get_year(book) for book in books
+								 if get_year(book) <= 2014)
+
+import matplotlib.pyplot as plt
+years = sorted(year_counts)
+book_counts = [year_counts[year] for year in years]
+plt.plot(years, book_counts)
+plt.ylabel("# of data books")
+plt.title("Data is Big")
+plt.show()
 
 
 
