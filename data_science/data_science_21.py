fa05b43375fd8903814ca53de0ec49aedcbc9498
Ryo Nozaki <ryoooo1128@gmail.com>
2018-05-28 13:53:18 -0700

    data_science_

--- data_science/data_science_21.py
+++ data_science/data_science_21.py
@@ -95,4 +95,95 @@ def farness(user):
 for user in users:
 	user["closest_certainlity"] = 1 / farness(user)
 
+
+#固有ベクトル中心性
+
+#行列操作
+def matrix_product_entry(A, B, i, j):
+	return dot(get_row(A, i), get_column(B, j))
+
+def matrix_multiply(A, B):
+	n1, k1 = shape(A)
+	n2, k2 = shape(B)
+	if k1 != n2:
+		raise ArithmeticError("incompatible shapes!")
+	return make_matrix(n1, k2, partial(matrix_product_entry, A, B))
+
+v = [1, 2, 3]
+v_as_matrix = [[1], [2], [3]]
+
+#この２つの表現を行き来するためのヘルパー関数を作る
+def vector_as_matrix(v):
+	return [[v_i] for v_i in v]#n*1行列のベクトルvを返す
+
+def vector_from_matrix(v_as_matrix):
+	return [row[0] for row in v_as_matrix]#n*1行列をリストとして返す
+
+#行列操作関数
+def matirix_operate(A, v):
+	v_as_matrix = vector_as_matrix(v)
+	product = matrix_multiply(A, v_as_matrix)
 	
+	return vector_from_matrix(product)
+
+
+def find_eigenvector(A, tolerance = 0.00001):
+	guess = [random.random() for _ in A]
+
+	while True:
+		result = matrix_operate(A, guess)
+		length = magnitude(result)
+		next_guess = scalar_multiply(1 / length, result)
+
+		if distance(guess, next_guess) < tolerance:
+			return next_guess, length
+
+		guess = next_guess
+#全てのベクトルが固有ベクトルを持つとは限らないが、find_eigenvectorが値を返したならそれは固有ベクトルとなる
+
+
+#中心性
+def entry_fn(i, j):
+	return 1 if (i, j) in friendships or (j, i) in friendships else 0:
+
+n = len(users)
+adjacency_matrix = make_matrix(n, n, entry_fn)
+
+
+#有効グラフとページランク
+endorsements = [(0, 1), (1, 0), (0, 2), (2, 0), (1, 2), (2, 1), (1, 3),
+                (2, 3), (3, 4), (5, 4), (5, 6), (7, 5), (6, 8), (8, 7), (8, 9)]
+
+for user in users:
+	user["endorses"] = []#ユーザーに「いいね！」の送信と受信を記録する
+	user["endorsed_by"] = []#リストをそれぞれ追加する
+
+for source_id, target_id, in endorsements:
+	user[source_id]["endorses"].append(user[target_id])
+	user[target_id]["endorsed_by"].append(user[source_id])
+
+endorsemennts_by_id = [(user["id"], len(user["endorsed_by"])) for user in users]
+
+sorted(endorsemennts_by_id, key = lambda (user_id, num_endorsements): num_endorsements, reverse = True)
+
+def page_ranked(user, damping = 0.8, num_iters = 100):
+	#最初にPageRankと等しく分配する
+	num_users = len(users)
+	pr = { user["id"] : 1 / num_users for user in users }
+
+	#ページランクの大部分の残りを、繰り返しごとに各ノードで分け合う
+	base_pr = (1 - damping) / num_users
+
+	for _ in range(num_iters):
+		next_pr = { user["id"] : base_pr for user in users }
+		for user in users:
+			#リンク先にページランクを分配する
+			links_pr = pr[user["id"] * damping]
+			for endorsee in user["endorses"]:
+				next_pr[endorsee["id"]] += links_pr / len(user["endorses"])
+
+
+		pr = next_pr
+
+	return pr
+
