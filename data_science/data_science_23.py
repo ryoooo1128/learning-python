50fe686c796c6a1946a01213a7892f96d570da95
Ryo Nozaki <ryoooo1128@gmail.com>
2018-05-30 23:24:03 -0700

    data_science

--- data_science/data_science_23.py
+++ data_science/data_science_23.py
@@ -57,3 +57,105 @@ class Table:
 	"""
 
 #行の更新
+	"""
+	Dunnに知り合いが増えた場合
+	UPDATE users
+	SET num_friends = 3
+	WHERE user_id = 1;
+	"""
+
+	def update(self, predicate = lambda row: True):
+		for row in self.rows:
+			if predicate(row):
+				for column, new_value in updates.items():
+					row[column] = new_value
+
+	users.update({'num_friends' : 3}, lambda row: row['user_id'] == 1)#user_id == 1の行だけnum_friends = 3にする
+
+
+#行の削除
+	"""
+	全ての行を削除
+	DELETE FROM users;
+	"""
+
+	"""
+	特定の行だけを削除する
+	DELETE FROM users WHERE user_id = 1;
+	"""
+
+	def delete(self, predicate = lambda row: True):
+		#全ての行、またはpredicateが指定された場合はそれに合致するものを削除する
+		self.rows = [row for row in self.rows if not (predicate(row))]
+
+		user.delete(lambda row: row["user_id"] == 1)#user_id == 1の行だけを削除する
+		user.delete()#全ての行を削除する
+
+
+#行の問い合わせ
+	"""
+	SELECT * FROM users;							#全ての中身を取り出す
+	SELECT * FROM users LIMIT 2;					#最初の2行だけ取り出す
+	SELECT user_id FROM users;						#特定の列のみを取り出す
+	SELECT user_id FROM users WHERE name = 'Dunn'	#特定の行のみを取り出す
+	
+	SELECT LENGTH(name) AS name_length FROM users;	#列の値を使った計算も可能
+	"""
+
+	def select(self, keep_columns = None, additional_columns = None):
+		#列が指定されていなければ全ての、列を返す
+		if keep_columns is None:
+			keep_columns = self.columns
+
+		if additional_columns is None:
+			additional_columns = {}
+
+		result_table = Table(keep_columns + additional_columns.keys())
+
+		for row in self.rows:
+			new_row = [row[column] for column in keep_columns]
+			for column_name, calculation in additional_columns.items():
+				new_row.append(calculation(row))
+			result_table.insert(new_row)
+
+		return result_table
+
+	def where(self, predicate = lambda row: True):
+		#指定したpreditcateに合致した行だけ返す
+		where_table = Table(self.columns)
+		where_table.rows = filter(predicate, self.rows)
+
+		return where_table
+
+
+	def limit(self, num_rows):
+		#先頭から一致した行数だけを返す
+		limit_table = Table(self.columns)
+		limit_table.rows = self.row[:num_rows]
+
+		return limit_table
+
+	#SELECT * FROM users;
+	users.select()
+	#SELECT * FROM users LIMIT 2;
+	users.limit(2)
+	#SELECT user_id FROM users;
+	users.select(keep_columns = ['user_id'])
+	#SELECT user_id FROM users WHERE name = "Dunn"
+	users.where(lambda row: row["name"] == "Dunn") \
+		 .select(keep_columns = ["user_id"])
+
+	#SELECT LENGTH(name) AS name_length FROM users:
+	def name_length(row): return len(row["name"])
+
+	users.select(keep_columns = [], additional_columns = { "name_length" : name_length})
+
+
+#グループ化
+	"""
+	SELECT LENGTH(name) as name_lenght, MIN(user_id) AS min_users, COUNT(*) AS num_users FROM users GROUP BY LENGTH(name);
+	
+	SELECT SUBSTR(name, 1, 1) AS first_letter, AVG(num_friends) AS avg_num_friends FROM users GROUP BY SUBSTR(name, 1, 1) HAVING AVG (num_friemds) > 1;
+
+	SELECT SUM(user_id) as user_id_sum FROM users WHERE user_id > 1;
+	"""
